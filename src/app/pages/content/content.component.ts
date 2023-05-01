d662334acb11d1262daa0987ed152e10beaee716
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-04-30 22:18:37 -0300

    feat: components have dynamic html insert

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
   photoCover: string = "https://uncensoredtactical.com/wp-content/uploads/2021/04/Placeholder-1920x1080-1.jpg"
   contentTitle: string = ""
   contentDescription: string = ""
 
-  constructor(){}
+  constructor(private route:ActivatedRoute ){
+
+  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value => console.log(value.get('id')))
+  }
 }
